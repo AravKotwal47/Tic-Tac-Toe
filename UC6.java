0863686e15395382f03a6a1d331378cddc4acfe7
AravKotwal <ak4746@srmist.edu.in>
2026-04-30 20:56:50 +0530

    Create UC6.java

---
+++ UC6.java
@@ -0,0 +1,23 @@
+public class UC6 {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+
+        // Sample move
+        placeMove(0, 0, 'X');
+
+        // Print the updated cell value
+        System.out.println(board[0][0]);
+    }
+
+    /**
+     * Updates the board by placing the given symbol at
+     * the specified row and column.
+     * Input: Row, Column, Symbol
+     * Hint: Assume the move is already validated.
+     */
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+}
